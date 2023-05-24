a041d8df2a46468099b4c25954269fb4f8a73efd
yanislavK <121428054+yanislavK@users.noreply.github.com>
2023-05-24 19:37:28 +0200

    upd video 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,15 +2,13 @@ import throttle from 'lodash.throttle';
 import Player from '@vimeo/player';
 //=====================================
 
-const videoPlayer = new Player('vimeo-player');
+const videoPlayer = new Player(`vimeo-player`);
 
 videoPlayer.on(
-  'timeupdate',
+  `timeupdate`,
   throttle(time => {
-    localStorage.setItem('videoplayer-current-time', time.seconds);
-
-    console.log(time);
+    localStorage.setItem(`update-current-time`, time.seconds);
   }, 1000)
 );
 
-videoPlayer.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+videoPlayer.setCurrentTime(localStorage.getItem(`update-current-time`))
